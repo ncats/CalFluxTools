3061bf465edecf134bb6245a68252bb955dcf042
Patt <andyptt21@gmail.com>
2022-09-16 14:29:11 -0400

    initial commit

---
+++ code/class_definitions.R
@@ -0,0 +1,20 @@
+#' @export
+setClass("plate", slots=list(
+  plateId="character",
+  format="numeric",
+  wellIds = "vector",
+  plateData = "data.frame",
+  plateDataMap = "list",
+  plateAnnotMap = "data.frame",
+  parameters = "vector"
+)
+)
+
+#' @export
+setClass("plateset", slots=list(
+  setname = "character",
+  plateIdList = "vector",
+  plateReplicateSets = "vector",
+  plates = "list"
+)
+)
---
+++ code/parser.R
@@ -0,0 +1,10 @@
+##' .. content for \description{} (no empty lines) ..
+##' Reads in 
+##' .. content for \details{} ..
+##' @title 
+##' @param filename (string) path to file to read in
+##' @return 
+##' @author Andrew Patt
+read_stemonix_data <- function(filename){
+  raw_data <- read.table(filename,fill=TRUE)
+}
